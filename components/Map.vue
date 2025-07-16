5a450ef6831557db9f9c98384766e88e4af0c961
AthanasiosChristopoulos <athanasioschristopoulos61@gmail.com>
2025-07-16 11:14:17 +0300

    Mapbox GL JS + GeoJSON dummy data

--- components/Map.vue
+++ components/Map.vue
@@ -1,16 +1,24 @@
 <template>
-    <h2>Clio Muse Map Explorer</h2>
     <div id="map" class="map-container"></div>
 </template>
 
 
 <script setup lang="ts">
-import { ref, onMounted, toRaw  } from 'vue'
+import { ref, onMounted  } from 'vue'
 import mapboxgl from 'mapbox-gl'
+import pin_image from '@/assets/icons/map-pin-fill.png';
 
 let map: mapboxgl.Map;
+let popup: mapboxgl.Popup;
 
-onMounted(() => {
+const geoData = ref({});
+
+onMounted(async() => {
+  const res = await fetch('http://localhost:5001/data')
+    .then(response => response.json())
+    .then(data => {
+        geoData.value = data;
+    });
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiZHMxMjNmMTUiLCJhIjoiY21jeGdvYnU0MGN3YzJsc2J4MWYzNGdkYiJ9.DoJqvXGIjVQcXt3Vx-n9AQ'
 
@@ -25,10 +33,47 @@ onMounted(() => {
     zoom: 5
   });
 
-  const marker = new mapboxgl.Marker({color: `#D54552`}).setLngLat([23.7261, 37.9715]).addTo(map); 
-  const marker_2 = new mapboxgl.Marker({ color: '#D54552'}).setLngLat([22.5010, 38.4808]).addTo(map);  
+  map.on('load', async () => {
+    try {
+      const response = await fetch(pin_image);
+      const blob = await response.blob();
 
+      const imageBitmap = await createImageBitmap(blob);
+
+      map.addImage('custom-pin', imageBitmap);
+
+      map.addSource('points', { type: 'geojson', data: geoData.value });
+
+      map.addLayer({
+        id: 'pin-layer',
+        type: 'symbol',
+        source: 'points',
+        layout: {
+          'icon-image': 'custom-pin',
+          'icon-size': 0.75,
+          'icon-anchor': 'bottom',
+          'icon-allow-overlap': true,
+        }
+      });
+    } catch (err) {
+      console.error('Error loading custom pin:', err);
+    }
+  });
+    
+  popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+    offset: 25           // little gap above the marker
+  }).setHTML(`
+      <div class="tooltip-title">
+        <h3>TourTitle</h3>
+      </div>
+      <p>This is your tooltip content</p>
+    `);
+
+  //===============================================================================================
   // Control:
+
   map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
 
   map.addControl(
@@ -47,12 +92,48 @@ onMounted(() => {
     showUserHeading: true
   }), 'top-right');
 
+  //===============================================================================================
+
   console.log('Viewport:', window.innerWidth + ' x ' + window.innerHeight);
 
+  //===============================================================================================
+  // Pin Events
+  map.on('mouseenter', 'pin-layer', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+
+    const feature = e.features?.[0];
+    const { id, title } = feature?.properties || {};
+    const coordinates = feature.geometry.coordinates;
+
+    setCurrentTour(id, coordinates);
+  });
+
+  map.on('mouseleave', 'pin-layer', () => {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
 
 });
 
+const tours = computed(() =>
+  geoData.value.features.map(f => ({
+    ...f.properties,                    
+  }))
+);
 
+function setCurrentTour(id, lngLat) {
+  const tour = tours.value.find(t => String(t.id) === String(id));
+
+  popup.setHTML(`
+    <div class="tooltip-title">
+      <h3>${tour.title}</h3>
+    </div>
+    <p>This is your tooltip content</p>
+  `).addTo(map).setLngLat(lngLat);
+};
+
+
+  
 </script>
 
 
