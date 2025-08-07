b4dc5bbb0a07819262d66b203fa2cadd48c919bc
AthanasiosChristopoulos <athanasioschristopoulos61@gmail.com>
2025-08-07 12:38:05 +0300

    Fix case sensitivity error

--- composables/useTooltip.js
+++ composables/useTooltip.js
@@ -1,5 +1,5 @@
-import { usePopup } from './tooltip/usePopup'
-import { useMapInteractions } from './tooltip/useMapInteractions'
+import { usePopup } from './tooltip/usePopup.js'
+import { useMapInteractions } from './tooltip/useMapInteractions.js'
 
 export function useTooltip(tours, map, findTours) {
   const { openTooltip, closeTooltip, getCurrentId, setExitAnimation } = usePopup(map, tours, findTours);
